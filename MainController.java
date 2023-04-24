426ebf93a5f0e3bf343254a757c2f627caff37da
DiegoEPH <dgiancottiorozco@gmail.com>
2023-04-24 21:29:57 +0200

    d

--- MainController.java
+++ MainController.java
@@ -10,28 +10,12 @@ public class MainController {
 
 
         printSomeNumbers();
-        printNumbers(5, 10);
-        int sum = sumUp(1, 5);
-        System.out.println("Summe: " + sum);
     }
 
     public static void printSomeNumbers() {
         for (int i = 6; i <= 23; i++) {
             System.out.println(i);
         }
-    }
 
-    public static void printNumbers(int start, int end) {
-        for (int i = start; i <= end; i++) {
-            System.out.println(i);
-        }
-    }
-
-    public static int sumUp(int start, int end) {
-        int sum = 0;
-        for (int i = start; i <= end; i++) {
-            sum += i;
-        }
-        return sum;
-    }
+}
 }
